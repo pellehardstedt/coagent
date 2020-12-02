745ed933b8af940ddc878e2b94237c50b8afba2d
pellehardstedt <pelle.hardstedt>
2020-12-02 19:34:14 +0100

    start of adding new contracts

--- src/coagent/ContractPanel.java
+++ src/coagent/ContractPanel.java
@@ -8,19 +8,28 @@ package coagent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 /**
  *
  * @author lenovo
  */
 public class ContractPanel extends javax.swing.JPanel {
-
     /**
      * Creates new form ContractPanel
      */
-    public ContractPanel() {
+    public ContractPanel() throws Exception {
         initComponents();
+        addComboBoxItems("Books_title", "books", 0);
+        addComboBoxItems("Authors_Name", "authors", 1);
+        addComboBoxItems("Clients_Name", "clients", 2);
+        addComboBoxItems("Editor_Name", "editors", 3);
+        addComboBoxItems("Publisher_Name", "publishers", 4);
+        addComboBoxItems("Agent_Username", "agents", 5);
+        
     }
 
     /**
@@ -32,17 +41,16 @@ public class ContractPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTableContracts1 = new javax.swing.JTable();
+        jScrollPaneTableSearch = new javax.swing.JScrollPane();
+        tableSearch = new javax.swing.JTable();
         jTextFieldContractSearch1 = new javax.swing.JTextField();
         jButtonSearchContracts1 = new javax.swing.JButton();
+        jScrollPaneTableAdd = new javax.swing.JScrollPane();
+        tableAdd = new javax.swing.JTable();
 
-        jTableContracts1.setModel(new javax.swing.table.DefaultTableModel(
+        tableSearch.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
+
             },
             new String [] {
                 "Title", "Author", "Client", "Editor", "Publisher", "Agent"
@@ -56,7 +64,7 @@ public class ContractPanel extends javax.swing.JPanel {
                 return types [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(jTableContracts1);
+        jScrollPaneTableSearch.setViewportView(tableSearch);
 
         jTextFieldContractSearch1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -71,6 +79,24 @@ public class ContractPanel extends javax.swing.JPanel {
             }
         });
 
+        tableAdd.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Title", "Author", "Client", "Editor", "Publisher", "Agent"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPaneTableAdd.setViewportView(tableAdd);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -78,11 +104,12 @@ public class ContractPanel extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jScrollPaneTableSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jTextFieldContractSearch1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jButtonSearchContracts1)))
+                        .addComponent(jButtonSearchContracts1))
+                    .addComponent(jScrollPaneTableAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(19, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -93,7 +120,9 @@ public class ContractPanel extends javax.swing.JPanel {
                     .addComponent(jTextFieldContractSearch1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButtonSearchContracts1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPaneTableSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPaneTableAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -130,8 +159,8 @@ public class ContractPanel extends javax.swing.JPanel {
             ResultSet result = query.executeQuery();
 
             // Removing Previous Data
-            while (jTableContracts1.getRowCount() > 0) {
-                ((DefaultTableModel) jTableContracts1.getModel()).removeRow(0);
+            while (tableSearch.getRowCount() > 0) {
+                ((DefaultTableModel) tableSearch.getModel()).removeRow(0);
             }
 
             //Creating Object []rowData for jTable's Table Model
@@ -143,19 +172,36 @@ public class ContractPanel extends javax.swing.JPanel {
                 {
                     row[i - 1] = result.getObject(i); // 1
                 }
-                ((DefaultTableModel) jTableContracts1.getModel()).insertRow(result.getRow() - 1,row);
+                ((DefaultTableModel) tableSearch.getModel()).insertRow(result.getRow() - 1,row);
             }
+            
+            //jTableContracts1.setValueAt("AAA", 0, 0);
 
         } catch (Exception e) {
             System.out.println(e);
         }
 
     }//GEN-LAST:event_jButtonSearchContracts1ActionPerformed
+    private void addComboBoxItems(String title, String table, int columnNumber) throws Exception{
+        TableColumn column = tableAdd.getColumnModel().getColumn(columnNumber);
+        JComboBox comboBox = new JComboBox();
+        Connection con = Coagent.getConnection();
+        PreparedStatement query = con.prepareStatement("SELECT " + title + " FROM " + table + ";");
+        ResultSet result = query.executeQuery();
 
+        
+        while(result.next()){
+            comboBox.addItem(result.getString(1));
+        }
+        
+        column.setCellEditor(new DefaultCellEditor(comboBox));
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonSearchContracts1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTableContracts1;
+    private javax.swing.JScrollPane jScrollPaneTableAdd;
+    private javax.swing.JScrollPane jScrollPaneTableSearch;
     private javax.swing.JTextField jTextFieldContractSearch1;
+    private javax.swing.JTable tableAdd;
+    private javax.swing.JTable tableSearch;
     // End of variables declaration//GEN-END:variables
 }
