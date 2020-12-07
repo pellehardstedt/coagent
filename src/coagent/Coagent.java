c2c7b12499608daed24d8dd17aa257d735495915
AxelKnutsson <axelkknutsson@gmail.com>
2020-12-07 14:14:47 +0100

    Uppdatering clients

--- src/coagent/Coagent.java
+++ src/coagent/Coagent.java
@@ -30,7 +30,7 @@ public class Coagent {
             String driver = "com.mysql.cj.jdbc.Driver";
             String url = "jdbc:mysql://localhost:3306/coagent?serverTimezone=UTC";
             String username = "root";
-            String password = "Tyronedaman";
+            String password = "";
             Class.forName(driver);
             Connection conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connected!");
